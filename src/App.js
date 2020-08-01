a2667fad97fe2586279dec9946a27abcd567ffec
Ala01 <alanfavero.montes1@gmail.com>
2020-08-01 15:02:28 -0300

    Setup3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Menu from './Components/Menu';
 import dadosIniciais from './data/dados_iniciais.json'
 import BannerMain from './Components/BannerMain';
-//import Carousel from './Components/Carousel';
+import Carousel from './Components/Carousel';
 import Footer from './Components/Footer';
 
 function App() {
@@ -16,7 +16,26 @@ function App() {
         url={dadosIniciais.categorias[0].videos[0].url}
       />
 
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
       
+      <Carousel 
+        category={dadosIniciais.categorias[1]}
+      />
+      
+      <Carousel 
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel 
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel 
+        category={dadosIniciais.categorias[4]}
+      />
 
       <Footer />
 
